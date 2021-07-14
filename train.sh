9eba5ad0f264d07878f0afb92db1bea9080cc5d8
Maria Grandury <mariagrandury@gmail.com>
2021-07-14 22:56:51 +0000

    Add more workers and specify cache_dir

--- train.sh
+++ train.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 ./run_wav2vec2_pretrain_flax.py \
-    --output_dir="./first_run" \
+    --output_dir="./third_run" \
     --num_train_epochs="5" \
     --per_device_train_batch_size="32" \
     --per_device_eval_batch_size="32" \
@@ -10,7 +10,7 @@
     --model_name_or_path="./" \
     --dataset_name="common_voice" \
     --dataset_config_name="es" \
-    --preprocessing_num_workers="4" \
+    --preprocessing_num_workers="96" \
     --max_duration_in_seconds="10.0" \
     --adam_beta1="0.9" \
     --adam_beta2="0.98" \
@@ -18,4 +18,5 @@
     --validation_split_percentage="5" \
     --speech_file_column="path" \
     --dtype="bfloat16" \
+    --cache_dir="./data_cache" \
     --push_to_hub
